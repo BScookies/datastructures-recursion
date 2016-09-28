af50e492ad07849f53fc60abd01e5e2f64f60d0b
= <=>
2016-09-28 14:26:10 -0600

    add myArray empty file

---
+++ myArray.js
@@ -0,0 +1,37 @@
+function MyArray() {
+  this.array = {};
+}
+
+MyArray.prototype.push = function(data) {
+  
+};
+MyArray.prototype.pop = function(data) {
+  
+};
+MyArray.prototype.shift = function(data) {
+  
+};
+MyArray.prototype.unshift = function(data) {
+  
+};
+MyArray.prototype.indexOf = function(data) {
+  
+};
+MyArray.prototype.findIndex = function(index) {
+  
+};
+MyArray.prototype.length = function() {
+  
+};
+MyArray.prototype.reverse = function() {
+  
+};
+MyArray.prototype.toString = function() {
+  
+};
+MyArray.prototype.forEach = function() {
+  
+};
+MyArray.prototype.map = function() {
+  
+};
